b13f68437767eacef4f16eda917fc6f1153aaedb
ogkourlias <orfeasgkourlias@gmail.com>
2023-02-11 22:59:18 +0100

    Added very basic wiki page.

---
+++ src/main/java/nl/d107/servlets/WikiServlet.java
@@ -0,0 +1,47 @@
+package nl.d107.servlets;
+
+import nl.d107.config.WebConfig;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "WikiServlet", urlPatterns = "/wiki", loadOnStartup = 1)
+public class WikiServlet extends HttpServlet {
+    // Stores an instance of the TemplateEngine
+    private TemplateEngine templateEngine;
+
+    /**
+     * Initialises the class: gets the TemplateEngine
+     * @throws ServletException if the servlet cannot be created
+     */
+    @Override
+    public void init() throws ServletException {
+        System.out.println("[Servlet] Initialising IndexServlet");
+        // Gets the template engine
+        this.templateEngine = WebConfig.getTemplateEngine();
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Configure the response
+        WebConfig.configureResponse(response);
+
+        // Create WebContext object
+        WebContext ctx = new WebContext(
+                request,
+                response,
+                request.getServletContext(),
+                request.getLocale());
+
+        // Process the template and data into a page
+        templateEngine.process("wiki", ctx, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+}
